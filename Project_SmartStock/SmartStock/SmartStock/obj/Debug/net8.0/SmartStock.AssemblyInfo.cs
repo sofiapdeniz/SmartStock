4c5ff19a795f6c1fcd3419033f7178ebfebab024
sofiapdeniz <sofiapdeniz05@gmail.com>
2025-12-01 13:13:37 -0300

    docker right way

--- Project_SmartStock/SmartStock/SmartStock/obj/Debug/net8.0/SmartStock.AssemblyInfo.cs
+++ Project_SmartStock/SmartStock/SmartStock/obj/Debug/net8.0/SmartStock.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartStock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f4447b344676114610635960092cba9892725de")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartStock")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartStock")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
